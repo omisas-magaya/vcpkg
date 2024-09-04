9a9334d48ae8b3e6af3c82f33fd9ee9b6069c9b0
Orlando Misas <30702136+omisas-magaya@users.noreply.github.com>
2024-09-04 17:58:09 -0400

    fix broken link

--- scripts/cmake/vcpkg_find_acquire_program.cmake
+++ scripts/cmake/vcpkg_find_acquire_program.cmake
@@ -282,6 +282,7 @@ function(vcpkg_find_acquire_program program)
         set(download_urls
             "https://download.qt.io/official_releases/jom/jom_1_1_3.zip"
             "https://mirrors.ocf.berkeley.edu/qt/official_releases/jom/jom_1_1_3.zip"
+            "https://qt.mirror.constant.com/official_releases/jom/jom_1_1_3.zip"
         )
         set(download_filename "jom_1_1_3.zip")
         set(download_sha512 5b158ead86be4eb3a6780928d9163f8562372f30bde051d8c281d81027b766119a6e9241166b91de0aa6146836cea77e5121290e62e31b7a959407840fc57b33)
